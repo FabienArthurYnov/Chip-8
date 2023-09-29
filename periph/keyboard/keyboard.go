dd23467195563157f913f260a68412c5ec7213d3
alexandre-jin <alexandrejin001@gmail.com>
2023-09-29 11:16:00 +0200

    keyboard done

--- periph/keyboard/keyboard.go
+++ periph/keyboard/keyboard.go
@@ -23,7 +23,7 @@ func SetupInput() rune {
 }
 
 
-func DetectedKey(char rune, keyPressed [16]bool) [16]bool {
+func DetectedKey(char rune, keyPressed []bool) []bool {
     // if err := keyboard.Open(); err != nil {
     //     fmt.Println(err)
     //     os.Exit(1)
@@ -37,81 +37,145 @@ func DetectedKey(char rune, keyPressed [16]bool) [16]bool {
 
 		// var keyPressed [16]bool
 
-        if char == '1' {
-            keyPressed[0x1] = true
-        } else {
-			keyPressed[0x1] = false
-		}
-        if char == '2' {
-            keyPressed[0x2] = true
-        } else {
-			keyPressed[0x2] = false
-		}
-        if char == '3' {
-            keyPressed[0x3] = true
-        } else {
-			keyPressed[0x3] = false
-		}
-        if char == 'q' || char == 'Q' {
-            keyPressed[0x4] = true
-        } else {
-			keyPressed[0x4] = false
-		}
-        if char == 'w' || char == 'W' {
-            keyPressed[0x5] = true
-        } else {
-			keyPressed[0x5] = true
-		}
-        if char == 'e' || char == 'E' {
-            keyPressed[0x6] = true
-        } else {
-			keyPressed[0x6] = false
-		}
-        if char == 'r' || char == 'R' {
-            keyPressed[0xd] = true
-        } else {
-			keyPressed[0xd] = false
-		}
-        if char == 'a' || char == 'A' {
-            keyPressed[0x7] = true
-        } else {
-			keyPressed[0x7] = false
-		}
-        if char == 's' || char == 'S' {
-            keyPressed[0x8] = true
-        } else {
-			keyPressed[0x8] = false
-		}
-        if char == 'd' || char == 'D' {
-            keyPressed[0x9] = true
-        } else {
-			keyPressed[0x9] = false
-		}
-        if char == 'f' || char == 'F' {
-            keyPressed[0xe] = true
-        } else {
-			keyPressed[0xe] = false
-		}
-        if char == 'z' || char == 'Z' {
-            keyPressed[0xa] = true
-        } else {
-			keyPressed[0xa] = false
-		}
-        if char == 'x' || char == 'X' {
-            keyPressed[0x0] = true
-        } else {
-			keyPressed[0x0] = false
-		}
-        if char == 'c' || char == 'C' {
-            keyPressed[0xb] = true
-        } else {
-			keyPressed[0xb] = false
-		}
-        if char == 'v' || char == 'V' {
-            keyPressed[0xf] = true
-        } else {
-			keyPressed[0xf] = false
-		}
 
+        for {
+            char, _, err := keyboard.GetKey()
+            if err != nil {
+                fmt.Println(err)
+                break
+            }
+    
+            if char == 'x' || char == 'X' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("0")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == '1' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("1")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == '2' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("2")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == '3' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("3")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == '4' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("c")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == 'q' || char == 'Q' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("4")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == 'w' || char == 'W' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("5")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == 'e' || char == 'E' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("6")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == 'r' || char == 'R' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("d")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == 'a' || char == 'A' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("7")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == 's' || char == 'S' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("8")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == 'd' || char == 'D' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("9")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == 'f' || char == 'F' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("e")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == 'z' || char == 'Z' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("a")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            
+            if char == 'c' || char == 'C' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("b")
+    
+            } else {
+                keyPressed = append(keyPressed, false)
+                // fmt.Println("mauvaise touche")
+            }
+            if char == 'v' || char == 'V' {
+                keyPressed = append(keyPressed, true)
+                // fmt.Println("f")
+    
+            } else {
+                // fmt.Println("mauvaise touche")
+                keyPressed = append(keyPressed, false)
+            }
+    
+            keyPressed = nil
+        }
     return keyPressed
 }
